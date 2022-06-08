a62f5fee17e66790a479c0107d8f54a1244946f0
Hussnain Ahmad <Husnainahmad521@gmail.com>
2022-06-08 21:17:21 +0500

    Announcement image field added

--- bapp/announcements/serializer.py
+++ bapp/announcements/serializer.py
@@ -2,7 +2,8 @@ from .models import Announcement
 from rest_framework import serializers
 
 class AnnouncementSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
     
     class Meta:
         model = Announcement
-        fields = '__all__'
+        fields = ['image','title','detail','Arthur','created_at']
