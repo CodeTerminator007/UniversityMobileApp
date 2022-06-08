9c2aa35013da4873932f4b51bf6a8d34464350e8
Hussnain Ahmad <Husnainahmad521@gmail.com>
2022-06-08 17:43:16 +0500

    Update urls.py

--- bapp/bapp/urls.py
+++ bapp/bapp/urls.py
@@ -11,6 +11,11 @@ from drf_yasg import openapi
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# ... the rest of your URLconf goes here ...
+
+
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -39,7 +44,7 @@ urlpatterns = [
   
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += staticfiles_urlpatterns()
 
 
 
